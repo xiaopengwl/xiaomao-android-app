567cfa3e2b9115365c52ed8af7b54b90d35e96e9
xiaopengwl <1604090592@qq.com>
2026-04-27 15:36:10 +0800

    UI readability refresh and add night mode: app/src/main/java/com/xiaomao/player/SettingsActivity.java

--- app/src/main/java/com/xiaomao/player/SettingsActivity.java
+++ app/src/main/java/com/xiaomao/player/SettingsActivity.java
@@ -14,9 +14,11 @@ public class SettingsActivity extends AppCompatActivity {
     private SwitchMaterial rememberSourceSwitch;
     private SwitchMaterial defaultLibrarySwitch;
     private SwitchMaterial keepSearchSwitch;
+    private SwitchMaterial nightModeSwitch;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        ThemeHelper.apply(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         bindViews();
@@ -29,10 +31,12 @@ public class SettingsActivity extends AppCompatActivity {
         rememberSourceSwitch = findViewById(R.id.settings_remember_source_switch);
         defaultLibrarySwitch = findViewById(R.id.settings_default_library_switch);
         keepSearchSwitch = findViewById(R.id.settings_keep_search_switch);
+        nightModeSwitch = findViewById(R.id.settings_night_mode_switch);
 
         backButton.setOnClickListener(v -> finish());
         doneButton.setOnClickListener(v -> {
             saveSettings();
+            ThemeHelper.apply(this);
             finish();
         });
     }
@@ -41,12 +45,14 @@ public class SettingsActivity extends AppCompatActivity {
         rememberSourceSwitch.setChecked(SettingsStore.rememberSource(this));
         defaultLibrarySwitch.setChecked(SettingsStore.defaultLibrary(this));
         keepSearchSwitch.setChecked(SettingsStore.keepLastSearch(this));
+        nightModeSwitch.setChecked(SettingsStore.nightModeEnabled(this));
     }
 
     private void saveSettings() {
         SettingsStore.setRememberSource(this, rememberSourceSwitch.isChecked());
         SettingsStore.setDefaultLibrary(this, defaultLibrarySwitch.isChecked());
         SettingsStore.setKeepLastSearch(this, keepSearchSwitch.isChecked());
+        SettingsStore.setNightModeEnabled(this, nightModeSwitch.isChecked());
         setResult(Activity.RESULT_OK);
     }
 }
